d8a206f386203722042c8c4dff7571fa431ee78f
Quentin Conner <quentinc@spiceworks.com>
2017-04-25 12:07:38 -0500

    JMX auth config bugfix.  version bump

--- src/ar/com/threelegs/newrelic/Defaults.java
+++ src/ar/com/threelegs/newrelic/Defaults.java
@@ -4,5 +4,5 @@ public class Defaults {
 	public static String CASSANDRA_PLUGIN_NAME = "ar.com.3legs.newrelic.cassandra";
 	public static String VARNISH_PLUGIN_NAME = "ar.com.3legs.newrelic.varnish";
 	public static String JMXREMOTE_PLUGIN_NAME = "ar.com.3legs.newrelic.jmxremote";
-	public static String VERSION = "2.0.0";
+	public static String VERSION = "3.0.0";
 }
